a92396f9dda6b3a355bd8b8ff254b9764824ea7d
Fitri Ratna Dewi <fratnadewi@gmail.com>
2022-02-27 21:15:35 +0700

    Use value from props

--- components/ResultCard.js
+++ components/ResultCard.js
@@ -1,24 +1,26 @@
 import React from 'react';
 
-const ResultCard = () => {
+const ResultCard = ({ totalBuy, importDuty, excises }) => {
   return (
-    <div className='w-1/2 mx-auto border-2 border-green-600 rounded-lg bg-white'>
-      <div className='p-4 grid place-items-center bg-green-600 text-white'>
-        <p>Hasil Perhitungan Pajak</p>
+    <div>
+      <div className='p-4 grid place-items-center bg-purple-500 text-white'>
+        <h2 className='text-lg text-center font-bold'>
+          Hasil Perhitungan Pajak
+        </h2>
       </div>
       <table className='table-auto w-full'>
         <tbody>
-          <tr className='border border-2 border-b-gray-300'>
+          <tr className=''>
             <td className='p-2'>Total Biaya Barang</td>
-            <td className='p-2 text-right'>0</td>
+            <td className='p-2 text-right'>{totalBuy}</td>
           </tr>
-          <tr className='border border-2 border-b-gray-300'>
+          <tr className=''>
             <td className='p-2'>Nilai Bea Masuk</td>
-            <td className='p-2 text-right'>0</td>
+            <td className='p-2 text-right'>{importDuty}</td>
           </tr>
           <tr>
             <td className='p-2'>Nilai PDRI</td>
-            <td className='p-2 text-right'>0</td>
+            <td className='p-2 text-right'>{excises}</td>
           </tr>
         </tbody>
       </table>
